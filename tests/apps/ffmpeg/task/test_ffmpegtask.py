beaa8e72e5af5628b6c1111e5f89c0b99095ddc2
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-04-05 15:32:26 +0200

    task: Plug video stream extraction and replacement into the workflow
    - Use _TC suffix for video-only merge+transcode output. Otherwise it can be easily confused with the output of the 'extract' step.

--- tests/apps/ffmpeg/task/test_ffmpegtask.py
+++ tests/apps/ffmpeg/task/test_ffmpegtask.py
@@ -175,12 +175,12 @@ class TestffmpegTask(TempDirFixture):
                 extra_data['track'],
                 os.path.join(
                     DockerJob.RESOURCES_DIR,
-                    'test.video[num=0].m3u8'))
+                    'test.video[video-only]_0.mp4'))
             self.assertEqual(
                 extra_data['output_stream'],
                 os.path.join(
                     DockerJob.OUTPUT_DIR,
-                    'test.video[num=0]_TC.m3u8'))
+                    'test.video[video-only]_0_TC.mp4'))
 
     def test_extra_data(self):
         ffmpeg_task = self._build_ffmpeg_task()
@@ -191,7 +191,7 @@ class TestffmpegTask(TempDirFixture):
         self.assertEqual(extra_data['script_filepath'],
                          '/golem/scripts/ffmpeg_task.py')
         self.assertEqual(extra_data['track'],
-                         '/golem/resources/test_video_0.mp4')
+                         '/golem/resources/test_video[video-only]_0.mp4')
         vargs = extra_data['targs']['video']
         aargs = extra_data['targs']['audio']
         self.assertEqual(vargs['codec'], d['options']['video']['codec'])
@@ -203,7 +203,7 @@ class TestffmpegTask(TempDirFixture):
         self.assertEqual(aargs['codec'], d['options']['audio']['codec'])
         self.assertEqual(aargs['bitrate'], d['options']['audio']['bit_rate'])
         self.assertEqual(extra_data['output_stream'],
-                         '/golem/output/test_video_0_TC.mp4')
+                         '/golem/output/test_video[video-only]_0_TC.mp4')
 
     def test_less_subtasks_than_requested(self):
         d = self._task_dictionary
