3d678980532d5fb3bb79bf63c013a5712dd9080d
Marek Franciszkiewicz <marek@golem.network>
2019-05-20 11:32:02 +0200

    Tests updated

--- tests/golem/test_client.py
+++ tests/golem/test_client.py
@@ -1102,7 +1102,7 @@ class TestClientRPCMethods(TestClientBase, LogTestCase):
 
     def test_golem_status_no_publisher(self, *_):
         component = 'component'
-        status = 'method', 'stage', 'data'
+        status = 'method', 'stage', {'status': 'message', 'value': 'data'}
 
         # status published, no rpc publisher
         StatusPublisher.publish(component, *status)
@@ -1111,7 +1111,7 @@ class TestClientRPCMethods(TestClientBase, LogTestCase):
     @inlineCallbacks
     def test_golem_status_with_publisher(self, *_):
         component = 'component'
-        status = 'method', 'stage', 'data'
+        status = 'method', 'stage', {'status': 'message', 'value': 'data'}
 
         # status published, with rpc publisher
         StatusPublisher._rpc_publisher = Mock()
